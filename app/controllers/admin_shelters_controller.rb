f460856ab85dde04e69e868f6e3961e293f7f430
Jake Kim <imjakekim@gmail.com>
2022-09-05 16:39:21 -0700

    admin show page contains address via raw sql

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -3,6 +3,7 @@ class AdminSheltersController < ApplicationController
   def show
     @shelter = Shelter.find(params[:id])
     @action_needed_pets = @shelter.action_needed_pets
+    @city = Shelter.city(@shelter.id)
   end
 
   def index
